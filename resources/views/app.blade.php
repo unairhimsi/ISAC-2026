19ea7f37280cfa6e91a484d1227b608d463199fb
misbahul45 <misbahulmuttaqin395@gmail.com>
2026-08-25 19:10:59 +0700

    Update logo image in public directory

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,7 +34,7 @@
     @if (config('seo.google_site_verification'))
         <meta name="google-site-verification" content="{{ config('seo.google_site_verification') }}">
     @endif
-    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" sizes="any">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png">
