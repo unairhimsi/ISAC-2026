0b026302c189f87ea2c38054d8298612306423ca
misbahul45 <misbahulmuttaqin395@gmail.com>
2026-09-17 23:28:32 +0700

    done

--- resources/js/Pages/Registration/Biodata.tsx
+++ resources/js/Pages/Registration/Biodata.tsx
@@ -390,7 +390,7 @@ const Biodata = () => {
 
   return (
     <div className="mx-auto w-full max-w-7xl px-3 text-center text-primary-foreground sm:px-4">
-      <div className="relative w-full">
+      <div className="relative w-full mt-12 md:mt-16">
         <div className="relative overflow-x-clip px-8 sm:px-10 md:px-12">
           <div className="grid w-full items-start">
             {members.map((member, index) => (
